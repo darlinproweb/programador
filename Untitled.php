52274e2beb80e98b3f724ccf530ebdaa89b404f9
darlinproweb <67169703+darlinproweb@users.noreply.github.com>
2020-07-06 13:29:47 -0400

    Add files via upload

---
+++ Untitled.php
@@ -0,0 +1,16 @@
+<?php
+		$destinatario = 'darlinprogramacionwed@gmail.com';
+		
+		$nombre = $_POST['nombre'];
+		$email = $_POST['email'];
+   	$mensaje = $_POST['mensaje'];
+		
+		$header = "Enviado a DarlinProWeb";
+		$mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+		
+		mail($destinatario, $mensajeCompleto, $header, email);
+		
+		echo "<script>alert('correo enviado exitosamente')</script>";
+		echo "<script> setTimeout(\"location.href='index.html'\",1000) </script>";
+		
+		?>
